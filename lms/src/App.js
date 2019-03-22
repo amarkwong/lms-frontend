8662e37aadc3e86ae1fa67f10cc3277ab84f0aff
amark_wong <amark.wong@gmail.com>
2019-03-23 01:12:04 +1000

    initial setup with saga and material ui

--- lms/src/App.js
+++ lms/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import ButtonAppBar from './components/appbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ButtonAppBar/>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
